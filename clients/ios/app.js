3fc2ba4c44323e035130eb8e5907a14db8a820c3
roche_d <roche_d@epitech.eu>
2015-05-11 21:34:23 +0200

    login screen

--- clients/ios/app.js
+++ clients/ios/app.js
@@ -2,4 +2,16 @@
  * Created by roche_d on 11/05/15.
  */
 
+// Initialize app
+var myApp = new Framework7();
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
 console.log('App started');
